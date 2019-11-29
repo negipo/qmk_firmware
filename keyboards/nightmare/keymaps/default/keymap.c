42ea234bc76d881a63c145cf241c45781d444716
negipo <negipo@gmail.com>
2019-11-29 21:57:29 +0900

    wip

--- keyboards/nightmare/keymaps/default/keymap.c
+++ keyboards/nightmare/keymaps/default/keymap.c
@@ -18,8 +18,8 @@
 #define OPT_TAB LCTL_T(KC_TAB)
 #define KC_CTLTB CTL_T(KC_TAB)
 #define KC_SFTMI RSFT_T(KC_MINUS)
-#define KC_ENONE LT(1, KC_ENTER)
 #define KC_SPTWO LT(2, KC_SPACE)
+#define KC_ENONE LT(1, KC_ENTER)
 #define KC_S1 LSFT(KC_1)
 #define KC_S2 LSFT(KC_2)
 #define KC_S3 LSFT(KC_3)
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split(/* Base */
                  RESET, KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLASH,
                  KC__VOLUP, KC_CTLTB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE, KC_BSPC,
-                 KC__VOLDOWN, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH,KC_SFTMI, KC_EQUAL,
+                 KC__VOLDOWN, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH,KC_SFTMI, LT(2, KC_EQUAL),
                  KC__MUTE, KC_LGUI, KC_LALT, KC_LGUI, KC_SPTWO, KC_ENONE, KC_RGUI, KC_RALT, KC_GRAVE),
 
     [1] = LAYOUT_split(
