157cdae9c54a1e5dec0984b084dfc4f9ec370ac0
Yoshiteru Negishi <negipo@gmail.com>
2021-01-30 12:58:12 +0900

    finish

--- keyboards/primekb/prime_e/keymaps/default/keymap.c
+++ keyboards/primekb/prime_e/keymaps/default/keymap.c
@@ -15,25 +15,43 @@
  */
 #include QMK_KEYBOARD_H
 
+#define KC_CTLTB CTL_T(KC_TAB)
+#define KC_SFTMI RSFT_T(KC_MINUS)
+#define KC_SFTEQL RSFT_T(KC_EQUAL)
+#define KC_SMINUS LSFT(KC_MINUS)
+#define KC_SEQUAL LSFT(KC_EQUAL)
+#define KC_SBSLASH LSFT(KC_BSLASH)
+#define KC_SGRAVE LSFT(KC_GRAVE)
+#define KC_S1 LSFT(KC_1)
+#define KC_S2 LSFT(KC_2)
+#define KC_S3 LSFT(KC_3)
+#define KC_S4 LSFT(KC_4)
+#define KC_S5 LSFT(KC_5)
+#define KC_S6 LSFT(KC_6)
+#define KC_S7 LSFT(KC_7)
+#define KC_S8 LSFT(KC_8)
+#define KC_S9 LSFT(KC_9)
+#define KC_S0 LSFT(KC_0)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
-		KC_ESC,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_DEL, KC_BSPC,
-		KC_TAB,   KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_QUOT, KC_ENT,
-		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-		KC_LCTL,  KC_LALT,                    MO(2), MO(1),	          KC_SPC, KC_RALT,                          KC_RGUI, KC_RCTL
+		KC_ESC,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+		KC_CTLTB,   KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_QUOTE,
+		KC_SFTEQL,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          KC_BSPC,  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_SFTMI,
+		KC__MUTE,  KC_LALT,           KC_LGUI, LT(1, KC_SPACE),	          LT(1, KC_ENTER), KC_RGUI,              LT(2, KC_BSLASH), LT(2, KC_GRAVE)
     ),
 
     LAYOUT(
-		KC_GRV,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_TRNS,    KC_UP,     KC_TRNS,   KC_MUTE,   KC_VOLD,   KC_VOLU,
-		KC_CAPS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_LEFT,    KC_DOWN,   KC_RIGHT,  KC_SCLN,   KC_BSLS,
-		KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_TRNS,    RESET,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-		BL_TOGG,   BL_STEP,                         KC_TRNS,    KC_TRNS,	        KC_TRNS,   KC_TRNS,                                     KC_TRNS,   KC_TRNS
+		KC_GRV,    KC_1,   KC_2,   KC_3,   KC_4,    KC_5,          KC_6,   KC_7,    KC_8,     KC_9,   KC_0,   KC_MINUS,   KC_EQUAL,
+		KC_CTLTB,   KC_S1,   KC_S2,   KC_S3,   KC_S4,    KC_S5,          KC_S6,   KC_S7,    KC_S8,   KC_S9,  KC_S0,   KC_SBSLASH,
+		KC_SFTEQL, KC_SBSLASH,KC_SGRAVE,KC_GRAVE,   KC_SEQUAL, KC_SMINUS,          KC_BSPC,   KC_TRNS,  KC_TRNS, KC_TRNS,KC_TRNS,KC_BSLASH, KC_SFTMI,
+		BL_TOGG,   BL_STEP,                         KC_LGUI,    KC_SPACE,	        KC_ENTER,   KC_RGUI,                                     KC_TRNS,   KC_TRNS
     ),
 
     LAYOUT(
-		KC_TRNS,  KC_1,      KC_2,      KC_3,      KC_4,       KC_5,             KC_6,      KC_7,     KC_8,    KC_9,    KC_0,      KC_MINS,  KC_EQL,
-		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_P4,    KC_P5,   KC_P6,   KC_LBRC,   KC_RBRC,
-		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_P1,    KC_P2,   KC_P3,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+		RESET,  KC_F1,      KC_F2,      KC_F3,      KC_F4,       KC_F5,             KC_F6,      KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_MINS,  KC_EQL,
+		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_P4,    KC_P5,   KC_UP,   KC_UP,   KC_RBRC,
+		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_P1,    KC_P2,   KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_TRNS,
 		KC_TRNS,  KC_TRNS,                         KC_TRNS,    KC_TRNS,	         KC_PENT,   KC_PDOT,                               KC_TRNS,  KC_TRNS
     ),
 
